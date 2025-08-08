5d083a46c92e520f8d28d3bcffa5c758ad2ae0b1
Mamadi Conteh <141883364+salone-web-dev@users.noreply.github.com>
2025-08-08 12:10:04 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "saferide-cache-v3";
+const urlsToCache = [
+  "https://saferide25.free.nf/",
+  "https://saferide25.free.nf/assets/css/saferide-style.css",
+  "https://saferide25.free.nf/assets/icons/icon-192.png",
+  "https://saferide25.free.nf/assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
