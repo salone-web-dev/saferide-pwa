083403931641ed35949fdd1d4436490c71752337
Mamadi Conteh <141883364+salone-web-dev@users.noreply.github.com>
2025-08-13 21:09:13 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,16 +7,27 @@ const urlsToCache = [
   "https://saferide25.free.nf/assets/icons/icon-512-v4.png"
 ];
 
+// Install - cache files
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// Fetch - serve from cache if available
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
-
+// Activate - remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
